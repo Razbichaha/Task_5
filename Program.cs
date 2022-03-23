dc9c62780953eed4e80630ae5dd20044c3e968b1
Сергей <seregabur@live.ru>
2022-03-23 21:49:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = 1;
+            int sekondValue = 2;
+            Console.WriteLine(" Первое значение = " + firstValue + "\n" + " Второе значение = " + sekondValue);
+            int tempValue = firstValue;
+            firstValue = sekondValue;
+            sekondValue = tempValue;
+            Console.WriteLine("\n"+" Переставим значения"+"\n");
+            Console.WriteLine(" Первое значение = " + firstValue + "\n" + " Второе значение = " + sekondValue);
+            Console.ReadLine();
+        }
+    }
+}
